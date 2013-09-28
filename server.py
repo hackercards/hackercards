20265841b89b232c253a1eebc6861b9cdd2b9b99
Brandon Pang <bjpang@berkeley.edu>
2013-09-28 16:16:44 -0700

    First hand works

--- server.py
+++ server.py
@@ -9,9 +9,6 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-black = create_deck('black.txt')
-white = create_deck('white.txt')
-
 
 def create_deck(file_name):
     with open(file_name) as f:
@@ -19,16 +16,29 @@ def create_deck(file_name):
     shuffle(color)
     return color
 
+
+black = create_deck('black.txt')
+white = create_deck('white.txt')
+counter = 1
+
+
+
+
 @app.route("/")
 def hello():
     return app.send_static_file('index.html')
 
-    
+
 @app.route("/ws")
 def cards():
+    global counter
     if request.environ.get('wsgi.websocket'):
+        cards = []
+        for x in range(7):
+            cards.append(black[counter])
+            counter += 1
+     
         ws = request.environ['wsgi.websocket']
-        cards =     
         message = {'type': 'join', 'hand': cards}
         ws.send(json.dumps(message))
 
