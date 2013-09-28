36c898cf6f46cde38f0786bfd33f947f886bcbcf
Brandon Pang <bjpang@berkeley.edu>
2013-09-28 14:15:57 -0700

    Add server file

---
+++ server.py
@@ -0,0 +1,24 @@
+from geventwebsocket.handler import WebSocketHandler
+from gevent.pywsgi import WSGIServer
+import sqlite3
+
+from flask import Flask 
+
+app = Flask(_name_)
+
+@app.route("/")
+def hello():
+	return app.send_static_file('index.html')
+
+
+@app.route("/ws")
+def cards():
+	if request.environ.get('wsgi.websocket'):
+		ws = request.environ['wsgi.websocket']
+		cards = ["Kyle", "Amy", "Brandon", "Andrew", "Cards"]
+		ws.send(json.dumps(cards))
+
+
+if __name__ == "__main__":
+    http_server = WSGIServer(('',5000), app, handler_class=WebSocketHandler)
+    http_server.serve_forever()
