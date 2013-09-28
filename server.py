0089c013c7595a0b61aea9a3fee51ef199786460
Kyle Raftogianis <kylerafto@gmail.com>
2013-09-28 16:44:06 -0700

    Create Game class

--- server.py
+++ server.py
@@ -7,43 +7,54 @@ import json
 from random import shuffle
 from flask import Flask, request
 
-app = Flask(__name__)
-
-
 def create_deck(file_name):
     with open(file_name) as f:
         color = f.readlines()
     shuffle(color)
     return color
 
+class Game:
+    def __init__(self):
+        self.black = create_deck('black.txt')
+        self.white = create_deck('white.txt')
+        self.counter = 1
 
-black = create_deck('black.txt')
-white = create_deck('white.txt')
-counter = 1
+        self.players = {}
 
 
+    def add_player(self, name, ws):
+        self.players[name] = ws
 
+        cards = []
+        for x in range(7):
+            cards.append(self.black[self.counter])
+            self.counter += 1
+
+        message = {'type': 'join', 'hand': cards}
+        ws.send(json.dumps(message))
+
+    def receive_message(name, msg):
+        pass
+
+
+app = Flask(__name__)
+
+game = Game()
 
 @app.route("/")
 def hello():
     return app.send_static_file('index.html')
 
-
 @app.route("/ws")
 def cards():
     global counter
     if request.environ.get('wsgi.websocket'):
-        cards = []
-        for x in range(7):
-            cards.append(black[counter])
-            counter += 1
-     
         ws = request.environ['wsgi.websocket']
         name = ws.receive()
-        message = {'type': 'join', 'hand': cards}
-        ws.send(json.dumps(message))
-
-
+        game.add_player(name, ws)
+        while True:
+            msg = ws.receive()
+            game.receive_message(name, msg)
 
 if __name__ == "__main__":
     http_server = WSGIServer(('',5000), app, handler_class=WebSocketHandler)
